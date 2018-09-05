122533c791b4f7b972255c8c1e401c0b6fd5fb7a
pvazteixeira <pvt@mit.edu>
2018-09-05 11:28:03 -0400

    updated pod files

--- bot2-frames/cmake/lcmtypes.cmake
+++ bot2-frames/cmake/lcmtypes.cmake
@@ -78,7 +78,7 @@
 #
 # ----
 # File: lcmtypes.cmake
-# Distributed with pods version: 12.09.21
+# Distributed with pods version: 17.02.11
 
 cmake_minimum_required(VERSION 2.6.0)
 
@@ -369,7 +369,7 @@ function(lcmtypes_build_java)
 
     # search for lcmtypes_*.jar files in well-known places and add them to the
     # classpath
-    foreach(pfx /usr /usr/local ${CMAKE_INSTALL_PREFIX})
+    foreach(pfx ${CMAKE_INSTALL_PREFIX} /usr /usr/local)
         file(GLOB_RECURSE jarfiles ${pfx}/share/java/lcmtypes_*.jar)
         foreach(jarfile ${jarfiles})
             set(java_classpath ${java_classpath}:${jarfile})
@@ -388,7 +388,7 @@ function(lcmtypes_build_java)
 
     # add a rule to build the .class files from from the .java files
     add_custom_command(OUTPUT ${_lcmtypes_class_files} COMMAND 
-        ${JAVA_COMPILE} -source 6 -target 6 -cp ${java_classpath} ${_lcmtypes_java_files} 
+        ${JAVA_COMPILE} -source 6 -cp ${java_classpath} ${_lcmtypes_java_files} 
         DEPENDS ${_lcmtypes_java_files} VERBATIM)
 
     # add a rule to build a .jar file from the .class files
